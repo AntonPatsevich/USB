f76728822fe2caa9e37af3867b7a3bce96a6fe64
AntonPatsevich <33219467+AntonPatsevich@users.noreply.github.com>
2019-12-18 18:00:20 +0300

    ALC
    ALCODOZE FINISHED OPERATIONS

--- AlcoDoze/AlcoDoze/MyForm.cpp
+++ AlcoDoze/AlcoDoze/MyForm.cpp
@@ -2,13 +2,12 @@
 
 using namespace System;
 using namespace System::Windows::Forms;
+[STAThread]
 void main(array<String^>^ arg) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	AlcoDoze::MyForm form; //WinFormsTest - имя вашего проекта
-	Application::Run(%form);
-	
+	Application::Run(%form);	
 }
-
 
 
--- AlcoDoze/AlcoDoze/MyForm.h
+++ AlcoDoze/AlcoDoze/MyForm.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Operations.h".
+#include "Operations.h";
 
 namespace AlcoDoze {
 
